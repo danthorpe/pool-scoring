8a4cefcde2e91f4629eb883dd285f407babe91c2
Daniel Thorpe <dan@blindingskies.com>
2011-12-10 20:07:08 +0000

    Gets a Hello World working.
    To run:
    $ bundle install
    $ backup -p 4567 config.ru
    
    Then visit http://localhost:4567/
    
    Signed-off-by: Daniel Thorpe <dan@blindingskies.com>

--- pool-scoring.rb
+++ pool-scoring.rb
@@ -1,15 +1,23 @@
-require 'json'
-require 'couchrest'
 require 'sinatra/base'
+require 'json/ext'
+require 'couchrest'
 
-Class PoolScoring < Sinatra::Base
+class PoolScoring < Sinatra::Base
 
-	# Define which CouchDB instance to use.
-	if ENV['CLOUDANT_URL']
-		set :db, ENV['CLOUDANT_URL'] + '/poolscoring'
-	else
-		set :db, 'http://localhost:5984/poolscoring'
-	end
+  # Define which CouchDB instance to use.
+  if ENV['CLOUDANT_URL']
+    set :db, ENV['CLOUDANT_URL'] + '/poolscoring'
+  else
+    set :db, 'http://localhost:5984/poolscoring'
+  end
 
+  set :root, File.dirname(__FILE__)
+  set :public_folder, 'public'
+  set :static, true
+
+  # Index page
+  get '/' do
+    "Hello World"
+  end
 
 end
