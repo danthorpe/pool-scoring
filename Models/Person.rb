88aef6f039d100026c2c9655a06104c3f2ea0059
Daniel Thorpe <dan@blindingskies.com>
2011-12-11 13:33:13 +0000

    Changes the accessors to be more Ruby-like.
    Signed-off-by: Daniel Thorpe <dan@blindingskies.com>

--- Models/Person.rb
+++ Models/Person.rb
@@ -8,14 +8,16 @@ class Person
   # @param doc A CouchDB document representing the person
   def initialize(doc)
     @doc = doc
+    
   end
-  
-  def name; @doc['name']; end
-  def username; @doc['username']; end
-  def email; @doc['email']; end
-  
+
+  # Use define_method for accessing the underlying CouchDB attributes
+  %w(_id name email username).each do | method |
+    define_method(method) { @doc[method.to_s] }
+  end
+    
   def to_s
-    "(#{@doc['username']}, #{@doc['email']})"
+    "(#{self.username}, #{self.email})"
   end
   
   def to_json
