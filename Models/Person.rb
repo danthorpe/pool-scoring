1db9f7b49138fa93db1d32f9f5c5baee7cfa4cc0
Rowan Manning <rowan@rowanmanning.co.uk>
2011-12-17 12:45:00 +0000

    Add a Gravatar class and integrate it into the Person Model

--- Models/Person.rb
+++ Models/Person.rb
@@ -1,25 +1,32 @@
 # @author Daniel Thorpe dan@blindingskies.com
 # @date 10/12/2011
 
+# Dependencies
+require './Library/Gravatar.rb'
+
 # Person class
-class Person  
+class Person
 
   # Constructor
   # @param doc A CouchDB document representing the person
   def initialize(doc)
     @doc = doc
-    
+    @avatar = Gravatar.new doc['email']
   end
 
   # Use define_method for accessing the underlying CouchDB attributes
   %w(_id name email username).each do | method |
     define_method(method) { @doc[method.to_s] }
   end
-    
+  
+  def avatar
+    @avatar
+  end
+
   def to_s
     "(#{self.username}, #{self.email})"
   end
-  
+
   def to_json
     @doc.to_json
   end
