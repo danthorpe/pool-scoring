c4bf7422ce4c9bdddd37108534aeb79c4e585ebb
Daniel Thorpe <dan@blindingskies.com>
2012-01-14 15:11:59 +0000

    Adds CouchDB view to get games between two players.
    Signed-off-by: Daniel Thorpe <dan@blindingskies.com>

--- CouchApp/Game/views/byPlayers/map.js
+++ CouchApp/Game/views/byPlayers/map.js
@@ -1,5 +1,9 @@
 function(doc) {
     if (doc.type == "Game") {
-        emit(doc.date, doc);
+        for (var i=0; i<doc.breakingTeam.length; i++) {
+            for (var j=0; j<doc.otherTeam.length; j++) {
+                emit([doc.breakingTeam[i], doc.otherTeam[j]], doc);
+            }
+        }
     }
 }
