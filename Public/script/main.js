bb2ba274ee37bcb3b03af11d80ffdf61a6c90c69
Rowan Manning <rowan@rowanmanning.co.uk>
2012-01-21 19:37:10 +0000

    Initial, not-so-pretty version of input disabling in game/add

--- Public/script/main.js
+++ Public/script/main.js
@@ -9,6 +9,45 @@
         // create header and close initially
         var header = new pool.Header($('#header'), $('#header a.toggle'));
         header.close();
+        
+        // team picker - super quick-and-dirty jQuery version...
+        // ... will fix this later :)
+        var teamPicker = $('#team-picker');
+        if (teamPicker.length > 0) {
+            
+            // loop players
+            $('label.player').each(function() {
+                
+                var player = $(this),
+                    playerId = player.attr('data-player-id'),
+                    input = player.find('input'),
+                    twin = teamPicker.find('[data-player-id="' + playerId + '"]').not(player);
+                
+                // input change
+                input.on('change', function(event) {
+                    if (input.is(':checked')) {
+                        player.addClass('recruited');
+                        twin.trigger('disable');
+                    } else {
+                        player.removeClass('recruited');
+                        twin.trigger('enable');
+                    }
+                    player.trigger('change');
+                }).trigger('change');
+                
+                // disable/enable
+                player.on('disable', function(event) {
+                    input.attr('disabled', true);
+                    player.addClass('disabled');
+                });
+                player.on('enable', function(event) {
+                    input.removeAttr('disabled');
+                    player.removeClass('disabled');
+                });
+                
+            });
+            
+        }
 
         // nasty nasty fix for ios web-apps added to the home screen - links
         // always open in a new safari window.
--- Public/script/pool/header.js
+++ Public/script/pool/header.js
@@ -68,7 +68,7 @@
         // return the constructor
         return Header;
         
-    })();
+    } ());
     
     // exports
     window.pool.Header = Header;
