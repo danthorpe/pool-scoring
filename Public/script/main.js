b43fb400894f7c52bce537445c751fd2e7b60dde
Rowan Manning <rowan@rowanmanning.co.uk>
2012-01-21 13:36:02 +0000

    Fixed header for mobile

--- Public/script/main.js
+++ Public/script/main.js
@@ -1,5 +1,8 @@
 (function(pool, $) {
 
+    // can haz javascript?
+    $('body').addClass('has-js');
+
     // create header and close initially
     var header = new pool.Header($('#header'), $('#header a.toggle'));
     header.close();
