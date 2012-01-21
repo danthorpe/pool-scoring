48e9703e7f9cc88474da9649ddee0e0163370426
Rowan Manning <rowan@rowanmanning.co.uk>
2012-01-21 14:06:56 +0000

    Add missing document load

--- Public/script/main.js
+++ Public/script/main.js
@@ -2,9 +2,14 @@
 
     // can haz javascript?
     $('body').addClass('has-js');
+    
+    // document load
+    $(function() {
 
-    // create header and close initially
-    var header = new pool.Header($('#header'), $('#header a.toggle'));
-    header.close();
+        // create header and close initially
+        var header = new pool.Header($('#header'), $('#header a.toggle'));
+        header.close();
+
+    });
 
 } (pool, jQuery));
