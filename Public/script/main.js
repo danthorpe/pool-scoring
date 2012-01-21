79acc51d4f83a744cc676626cddd38c8e7d7bc6a
Rowan Manning <rowan@rowanmanning.co.uk>
2012-01-21 14:22:04 +0000

    iOS web-apps no longer open Safari on link clicks

--- Public/script/main.js
+++ Public/script/main.js
@@ -1,7 +1,7 @@
 (function(pool, $) {
 
     // can haz javascript?
-    $('body').addClass('has-js');
+    var body = $('body').addClass('has-js');
     
     // document load
     $(function() {
@@ -9,6 +9,18 @@
         // create header and close initially
         var header = new pool.Header($('#header'), $('#header a.toggle'));
         header.close();
+
+        // nasty nasty fix for ios web-apps added to the home screen - links
+        // always open in a new safari window.
+        if (navigator.standalone !== undefined && navigator.standalone === true) {
+            body.on('click', 'a', function(event) {
+                var href = $(this).attr('href');
+                if (href[0] !== '#') {
+                    window.location = $(this).attr('href');
+                    event.preventDefault();
+                }
+            });
+        }
 
     });
 
