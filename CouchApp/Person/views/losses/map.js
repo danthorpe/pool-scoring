b05b3fbfe8eecfeddca1bf127e7d707df7410f32
Dan Thorpe <dan@bravenewtalent.com>
2012-01-24 13:50:10 +0000

    Only computes states on a rolling 7 day basis.
    Signed-off-by: Dan Thorpe <dan@bravenewtalent.com>

--- CouchApp/Person/views/losses/map.js
+++ CouchApp/Person/views/losses/map.js
@@ -1,12 +1,17 @@
 function(doc) {
     if (doc.type == "Game") {
-        if (doc.breakingTeamWon) {
-            for (var i = 0; i < doc.otherTeam.length; i++) {
-                emit(doc.otherTeam[i], 1);
-            }
-        } else {
-            for (var i = 0; i < doc.breakingTeam.length; i++) {
-                emit(doc.breakingTeam[i], 1);
+        now = Date.now() / 1000.0;
+        difference = now - doc.date;
+
+        if (difference < 604800) {        
+            if (doc.breakingTeamWon) {
+                for (var i = 0; i < doc.otherTeam.length; i++) {
+                    emit(doc.otherTeam[i], 1);
+                }
+            } else {
+                for (var i = 0; i < doc.breakingTeam.length; i++) {
+                    emit(doc.breakingTeam[i], 1);
+                }
             }
         }
     }
