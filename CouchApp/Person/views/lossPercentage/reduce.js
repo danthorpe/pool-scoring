6e6b244f1d367ffa0deaf175e42135baa7e9d9c7
Daniel Thorpe <dan@blindingskies.com>
2012-01-28 17:02:25 +0000

    Removes unnecessary CouchDB Views.
    Signed-off-by: Daniel Thorpe <dan@blindingskies.com>

--- CouchApp/Person/views/lossPercentage/reduce.js
+++
@@ -1,7 +0,0 @@
-function (keys, values, rereduce) {
-    var wins = 0;
-    for (var i=0; i<values.length; i++) {
-        wins += values[i][1];
-    }
-    return (wins / values.length) * 100.0;
-}
--- CouchApp/Person/views/winPercentage/reduce.js
+++
@@ -1,7 +0,0 @@
-function (keys, values, rereduce) {
-    var wins = 0;
-    for (var i=0; i<values.length; i++) {
-        wins += values[i][0];
-    }
-    return (wins / values.length) * 100.0;
-}
