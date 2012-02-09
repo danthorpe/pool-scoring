fb8008bbd6f74b7ef2857679bced3da065d8cd08
Daniel Thorpe <dan@blindingskies.com>
2012-02-09 00:22:55 +0000

    Puts the leaderboard back to normal
    Signed-off-by: Daniel Thorpe <dan@blindingskies.com>

--- CouchApp/Game/views/leaderboard/map.js
+++ CouchApp/Game/views/leaderboard/map.js
@@ -3,17 +3,17 @@ function(doc) {
             
         if (doc.breakingTeamWon) {
             for (var i=0; i<doc.breakingTeam.length; i++) {
-                emit([doc.date, doc.breakingTeam[i]], 1.0 / doc.breakingTeam.length);
+                emit([doc.breakingTeam[i], doc.date], 1.0 / doc.breakingTeam.length);
             }
             for (var i=0; i<doc.otherTeam.length; i++) {
-                emit([doc.date, doc.otherTeam[i]], -1.0 / doc.otherTeam.length);
+                emit([doc.otherTeam[i], doc.date], -1.0 / doc.otherTeam.length);
             }
         } else {
             for (var i=0; i<doc.breakingTeam.length; i++) {
-                emit([doc.date, doc.breakingTeam[i]], -1.0 / doc.breakingTeam.length);
+                emit([doc.breakingTeam[i], doc.date], -1.0 / doc.breakingTeam.length);
             }
             for (var i=0; i<doc.otherTeam.length; i++) {
-                emit([doc.date, doc.otherTeam[i]], 1.0 / doc.otherTeam.length);
+                emit([doc.otherTeam[i], doc.date], 1.0 / doc.otherTeam.length);
             }
         }
     }
